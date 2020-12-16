565cb3f1f4f969d3541bf1b94cd4a4fd7b8ad604
lhx <1937506994@qq.com>
2020-12-16 09:41:08 +0800

    测试版本控制代码提交

---
+++ src/main/java/com/java/konwledge/basicinfo/util/CheckMasterTest.java
@@ -0,0 +1,10 @@
+package com.java.konwledge.basicinfo.util;
+
+/**
+ *  @dept 上海软件研发中心
+ *  @description 这是测试版本控制是否存在区别的测试
+ *  @author HaoXin.Liu
+ *  @date 2020/12/16 9:39
+ **/
+public class CheckMasterTest {
+}
